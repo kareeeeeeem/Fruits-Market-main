9e0f040c6429e182d1828b6fdb0beefab2a63bdb
kareem emad <88625235+kareeeeeeem@users.noreply.github.com>
2023-05-15 16:03:16 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_market/features/Auth/presentation/manger/cubit/auth_cubit.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'features/splash/presentation/splash_view.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(FruitsMarket());
+}
+
+class FruitsMarket extends StatelessWidget {
+  const FruitsMarket({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData(fontFamily: 'Poppins'),
+      debugShowCheckedModeBanner: false,
+      home: BlocProvider(
+        create: (context) => AuthCubit(),
+        child: SplashView(),
+      ),
+    );
+  }
+}
